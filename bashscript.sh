2ad8ec6a2a0de13742928ba30a7c6bfec2ae0ebf
Humberto <humberto.peluso@yahoo.com.br>
2023-09-18 14:39:52 -0300

    testing bash script test pipeline creation.

--- bashscript.sh
+++ bashscript.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo 'Hello World!'
+
+curl -X POST http://${BASIC_AUTH}@${JENKINS_ADDRESS}/createItem?name=pipeline-test \
+    --header "Content-Type:text/xml" \
+    --data-binary @config.xml
